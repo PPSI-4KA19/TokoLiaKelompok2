5651274285fe271260515da4db2729defd161f6a
david <david_wawarag@yahoo.com>
2023-12-08 22:23:58 +0700

    add UI halaman kategori

--- app/src/main/java/com/example/tokolia/produkkategori.java
+++ app/src/main/java/com/example/tokolia/produkkategori.java
@@ -3,12 +3,31 @@ package com.example.tokolia;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.GridView;
+import android.widget.TextView;
 
 public class produkkategori extends AppCompatActivity {
+
+    GridView gridKategori;
+    Button buttonTambahProduk;
+    Button buttonTambahKategori;
+    TextView namaKategori;
+    TextView descKategori;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_produkkategori);
+
+        gridKategori = findViewById(R.id.gridKategori);
+
+        buttonTambahKategori = findViewById(R.id.buttonTambahKategori);
+        buttonTambahProduk = findViewById(R.id.buttonTambahProduk);
+
+        //untuk ditulis ke db dalam adapter
+        //namaKategori = findViewById(R.id.textNamaKategori);
+        //descKategori = findViewById(R.id.textDescKategori);
+
     }
 }
