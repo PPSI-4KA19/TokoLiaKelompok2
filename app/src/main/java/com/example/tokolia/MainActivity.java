557a62832e33d0259b4d11753b3474e1184108ed
david <david_wawarag@yahoo.com>
2023-12-17 16:52:18 +0700

    add overflowaction

--- app/src/main/java/com/example/tokolia/MainActivity.java
+++ app/src/main/java/com/example/tokolia/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.tokolia;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.content.res.AppCompatResources;
 
 import android.os.Bundle;
 import android.widget.TextView;
+import android.widget.Toolbar;
+
+import com.google.android.material.appbar.MaterialToolbar;
 
 public class MainActivity extends AppCompatActivity {
 
+    MaterialToolbar toolbar;
     TextView textTahun;
     TextView nominalPemasukan;
     TextView nominalPengeluaran;
@@ -15,6 +20,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //toolbar setup
+        toolbar = findViewById(R.id.mainToolbar);
+        toolbar.setOverflowIcon(AppCompatResources.getDrawable(this,R.drawable.baseline_menu_24));
+
 
         //text tahun
         textTahun = findViewById(R.id.textViewTahun);
