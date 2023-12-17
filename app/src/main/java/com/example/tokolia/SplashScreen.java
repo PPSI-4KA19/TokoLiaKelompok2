a5f3e97c5a9eeb9808bbfb0e914ff31a1550324f
david <david_wawarag@yahoo.com>
2023-12-17 11:40:15 +0700

    add splash/load screen

---
+++ app/src/main/java/com/example/tokolia/SplashScreen.java
@@ -0,0 +1,42 @@
+package com.example.tokolia;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.widget.ImageView;
+import android.widget.TextView;
+
+public class SplashScreen extends AppCompatActivity {
+
+    ImageView image;
+    TextView loadText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+
+        //define items
+        image = findViewById(R.id.imageViewSplash);
+        loadText = findViewById(R.id.textViewSplash);
+
+        //load animasi
+        Animation animation = AnimationUtils.loadAnimation(getApplicationContext(),R.anim.splash_anim);
+        loadText.startAnimation(animation);
+
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+
+                Intent openMain = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(openMain);
+                finish();
+
+            }
+        },3000);
+    }
+}
